b3256332b04ad69dcad1d0f9c2e5843d4118bd98
Thusikaran <pushparasa.thusikaran@gmail.com>
2024-07-01 10:58:33 +0530

    first commit

---
+++ src/components/TrainingComponent/Training.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Training.css'
+import Header from '../layout/Header'
+
+const Training = () => {
+  return (
+    <div className='training-body'>
+        <Header/>
+        <hr />
+        <div>
+            <div>
+              <h5>NEW COURSES</h5>
+              <p>View All {'>>>'}</p>
+            </div>
+            
+            <div>
+                card
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Training
