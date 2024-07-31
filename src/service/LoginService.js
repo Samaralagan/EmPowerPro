51f76a91879e5e2a7e4dffa3ec0a84fee2701834
Inscode <inshafahmedh@gmail.com>
2024-07-31 18:22:45 +0530

    login connection

---
+++ src/service/LoginService.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+const BASE_URL_EMPLOYEE = "http://localhost:8080";
+
+export const checkLogin = (username, password) => axios.post(BASE_URL_EMPLOYEE+"/api/auth/login", {username, password});
+
+export const isAuthenticated = () => {
+    const token = localStorage.getItem('token');
+    return !!token;
+};
+
+export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('role')
+};
+
+export const isAdmin = () => {
+    const role = localStorage.getItem('role');
+    return role === 'ADMIN'
+
+}
+
+export const isHr = () => {
+    const role = localStorage.getItem('role');
+    return role === 'HR'
+
+}
+export const isExecutive = () => {
+    const role = localStorage.getItem('role');
+    return role === 'ADMIN'
+
+}
+export const isTeamLead = () => {
+    const role = localStorage.getItem('role');
+    return role === 'TEAMLEAD'
+
+}
+export const isFinace = () => {
+    const role = localStorage.getItem('role');
+    return role === 'FINANCE'
+
+}
+
+export const isEmployee = () => {
+    const role = localStorage.getItem('role');
+    return role === 'EMPLOYEE'
+
+}
