d16ff29320cb262103307bb4f7758ee7a6413f23
Malki2001 <malkiyasodhara18@gmail.com>
2024-11-13 19:12:46 +0530

    make the validations in contact us

--- src/components/HomeComponent/Contactus.jsx
+++ src/components/HomeComponent/Contactus.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React , { useState } from 'react'
 import './Contactus.css'
 import { CONTACTUS } from '../constants/contents'
 import { SOCIALMEDIA } from '../constants/contents'
 
 const Contactus = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [contactNo, setContactNo] = useState('');
+    const [description, setDescription] = useState('');
+    const [errors, setErrors] = useState({});
+  
+    const namePattern = /^[A-Za-z]+$/;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const phonePattern = /^(\+94)?7\d{8}$/;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newErrors = {};
+    
+        if (!firstName) {
+          newErrors.firstName = 'Please enter your First Name!';
+        } else if (!namePattern.test(firstName)) {
+          newErrors.firstName = 'First Name can only contain letters!';
+        }
+    
+        if (!lastName) {
+          newErrors.lastName = 'Please enter your Last Name!';
+        } else if (!namePattern.test(lastName)) {
+          newErrors.lastName = 'Last Name can only contain letters!';
+        }
+    
+        if (!email) {
+          newErrors.email = 'Please enter your Email!';
+        } else if (!emailPattern.test(email)) {
+          newErrors.email = 'Please enter a valid Email!';
+        }
+    
+        if (!contactNo) {
+          newErrors.contactNo = 'Please enter your Contact No!';
+        } else if (!phonePattern.test(contactNo)) {
+          newErrors.contactNo = 'Please enter a valid Contact No!';
+        }
+    
+        if (!description) {
+          newErrors.description = 'Please provide a description!';
+        }
+    
+        setErrors(newErrors);
+    
+        if (Object.keys(newErrors).length === 0) {
+          console.log('Form submitted successfully');
+         
+        }
+      };
+    
   return (
     <div className='contactus-body'>
         <div className='d-flex'>
@@ -11,42 +62,78 @@ const Contactus = () => {
                 <h2 className='contactus-title'>{CONTACTUS.title}</h2>
                 <div className='contactus-content'>{CONTACTUS.text}</div>
                 
-                <form action="" className='contactus-form'>
+                <form action="" className='contactus-form' onSubmit={handleSubmit} >
                     <div className='contactus-form-row'>
                         <div className='contactus-form-col'>
                             <div className='contactus-form-input-box'>
-                                <label htmlFor="firstName">First Name</label>
-                                <input type="text" placeholder='Kasun' />
-                            </div>
-                        </div>
-                        <div className='contactus-form-col'>
+                                  <label htmlFor="firstName">First Name</label>
+                                  <input
+                                      type="text"
+                                      placeholder="Kasun"
+                                      value={firstName}
+                                      onChange={(e) => setFirstName(e.target.value)}
+                                      className={errors.firstName ? 'input-error' : ''}
+                                  />
+                                   {errors.firstName && <div className="error">{errors.firstName}</div>}
+                              </div>
+                          </div>
+
+                          <div className='contactus-form-col'>
                             <div className='contactus-form-input-box'>
-                                <label htmlFor="firstName">Last Name</label>
-                                <input type="text" placeholder='Fernando' />
-                            </div>
-                        </div>
+                                  <label htmlFor="firstName">Last Name</label>
+                                  <input
+                                      type="text"
+                                      placeholder="Fernando"
+                                      value={lastName}
+                                      onChange={(e) => setLastName(e.target.value)}
+                                      className={errors.lastName ? 'input-error' : ''}
+                                  />
+                                  {errors.lastName && <div className="error">{errors.lastName}</div>}
+                              </div>
+                          </div>
                      </div>
+
                     <div className='contactus-form-row'>
                         <div className='contactus-form-col'>
                             <div className='contactus-form-input-box'>
                                 <label htmlFor="firstName">Email Address</label>
-                                <input type="text" placeholder='kasunfernando1990@gmail.com' />
-                            </div>
-                        </div>
+                                  <input
+                                      type="text"
+                                      placeholder="kasunfernando1990@gmail.com"
+                                      value={email}
+                                      onChange={(e) => setEmail(e.target.value)}
+                                      className={errors.email ? 'input-error' : ''}
+                                  />
+                                  {errors.email && <div className="error">{errors.email}</div>}
+                              </div>
+                          </div>
                         <div className='contactus-form-col'>
                             <div className='contactus-form-input-box'>
-                                <label htmlFor="firstName">Contact No.</label>
-                                <input type="text" placeholder='+94 777 808 6754' />
-                            </div>
-                        </div>
-                    </div>
+                                  <label htmlFor="firstName">Contact No.</label>
+                                  <input
+                                      type="text"
+                                      placeholder="+94 777 808 675"
+                                      value={contactNo}
+                                      onChange={(e) => setContactNo(e.target.value)}
+                                      className={errors.contactNo ? 'input-error' : ''}
+                                  />
+                                  {errors.contactNo && <div className="error">{errors.contactNo}</div>}
+                              </div>
+                          </div>
+                      </div>
                     <div className='contactus-form-row'>
                         <div className='contactus-form-input-box'>
                              <label htmlFor="firstName" className='ms-4'>Description</label>
-                            <textarea type="text" placeholder='I would prefer if you ........' />
-                        </div>
-                    </div>
-                    <div className='contactus-form-button'>
+                              <textarea
+                                  placeholder="I would prefer if you ........"
+                                  value={description}
+                                  onChange={(e) => setDescription(e.target.value)}
+                                  className={errors.description ? 'input-error' : ''}
+                              />
+                              {errors.description && <div className="error">{errors.description}</div>}
+                          </div>
+                      </div>
+                      <div className='contactus-form-button'>
                         <button className='gradient-blue-btn '>SUBMIT</button>
                     </div>
                 </form>
