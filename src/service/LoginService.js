0ce899bc1728a2ddaf1bec96519714751310ba05
Inscode <inshafahmedh@gmail.com>
2025-01-27 20:55:23 +0530

    correct login api

--- src/service/LoginService.js
+++ src/service/LoginService.js
@@ -2,47 +2,42 @@ import axios from "axios";
 
 const BASE_URL_EMPLOYEE = "http://localhost:8080";
 
-export const checkLogin = (username, password) => axios.post(BASE_URL_EMPLOYEE+"/api/auth/login", {username, password});
+export const checkLogin = (username, password) =>
+  axios.post(BASE_URL_EMPLOYEE + "/api/v1/auth/login", { username, password });
 
 export const isAuthenticated = () => {
-    const token = localStorage.getItem('token');
-    return !!token;
+  const token = localStorage.getItem("token");
+  return !!token;
 };
 
 export const logout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('role')
+  localStorage.removeItem("token");
+  localStorage.removeItem("role");
 };
 
 export const isAdmin = () => {
-    const role = localStorage.getItem('role');
-    return role === 'ADMIN'
-
-}
+  const role = localStorage.getItem("role");
+  return role === "ADMIN";
+};
 
 export const isHr = () => {
-    const role = localStorage.getItem('role');
-    return role === 'HR'
-
-}
+  const role = localStorage.getItem("role");
+  return role === "HR";
+};
 export const isExecutive = () => {
-    const role = localStorage.getItem('role');
-    return role === 'ADMIN'
-
-}
+  const role = localStorage.getItem("role");
+  return role === "ADMIN";
+};
 export const isTeamLead = () => {
-    const role = localStorage.getItem('role');
-    return role === 'TEAMLEAD'
-
-}
+  const role = localStorage.getItem("role");
+  return role === "TEAMLEAD";
+};
 export const isFinace = () => {
-    const role = localStorage.getItem('role');
-    return role === 'FINANCE'
-
-}
+  const role = localStorage.getItem("role");
+  return role === "FINANCE";
+};
 
 export const isEmployee = () => {
-    const role = localStorage.getItem('role');
-    return role === 'EMPLOYEE'
-
-}
+  const role = localStorage.getItem("role");
+  return role === "EMPLOYEE";
+};
